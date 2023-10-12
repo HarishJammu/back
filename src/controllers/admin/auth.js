7553f2eb838c79aa2b4065c81c9279f3aa7bbc94
HarishJammu <105035835+HarishJammu@users.noreply.github.com>
2023-10-12 18:18:00 +0530

    Update auth.js

--- src/controllers/admin/auth.js
+++ src/controllers/admin/auth.js
@@ -45,7 +45,7 @@ exports.signin=(req,res)=>{
         if(error) return res.status(400).json({error})
         if(user){
             if(user.authenticate(req.body.password) && user.role==="admin"){
-                const token=jwt.sign({_id:user._id,role:user.role},("MERNSECRETE"),{expiresIn:"1d"})
+                const token=jwt.sign({_id:user._id,role:user.role},(process.env.JWT_SECRET),{expiresIn:"1d"})
 
                 const {_id,firstName,secondName,email,role,fullName}=user;
                 res.cookie('token', token,{expiresIn:'1d'});
